e8b27f401316d980a0e12c9004e643a0ab915687
hongggs <hongseoda@gmail.com>
2022-07-28 18:16:55 +0900

    modfiy: 설문조사 반환 성공메세지로 수정

--- withplanner_api/src/main/java/withplanner/withplanner_api/controller/InvestigationController.java
+++ withplanner_api/src/main/java/withplanner/withplanner_api/controller/InvestigationController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import withplanner.withplanner_api.dto.ResultMsgResp;
 import withplanner.withplanner_api.dto.investigation.InvestReq;
 import withplanner.withplanner_api.service.InvestigationService;
 
@@ -15,7 +16,7 @@ public class InvestigationController {
 
     private final InvestigationService investigationService;
     @PostMapping("/investigation")
-    public String invest(@RequestBody InvestReq investReq) {
+    public ResultMsgResp invest(@RequestBody InvestReq investReq) {
         return investigationService.invest(investReq);
     }
 }
