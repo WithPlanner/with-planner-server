4a39a30d31c58e8b86eb1c8d1f6045455d42f32a
hongggs <hongseoda@gmail.com>
2022-08-11 04:38:48 +0900

    modify: 커뮤니티를 생성할때 사진이 null이면 디폴트를 생성해줌

--- withplanner_api/src/main/java/withplanner/withplanner_api/service/CommunityService.java
+++ withplanner_api/src/main/java/withplanner/withplanner_api/service/CommunityService.java
@@ -38,35 +38,26 @@ public class CommunityService {
     @Transactional
     public ResultLongResp createMapCommunity(CommunityMakeReq reqDto, String username) {
         Community community;
-
+        String imgUrl = "https://withplanner-s3.s3.ap-northeast-2.amazonaws.com/default_communityImg_transparency.png";
         User user = userRepository.findByEmail(username)
                 .orElseThrow(() -> new BaseException(BaseResponseStatus.NOT_EXISTS_PARTICIPANT));
 
         if (reqDto.getCommunityImg() != null) {
             String name = s3Service.uploadToAWS(reqDto.getCommunityImg());
             //s3에 저장된 imgUrl 이를 저장하면 된다.
-            String imgUrl = "https://withplanner-s3.s3.ap-northeast-2.amazonaws.com/" + name;
-            community = Community.builder()
-                    .name(reqDto.getName())
-                    .introduce(reqDto.getIntroduce())
-                    .communityImg(imgUrl)
-                    .headCount(reqDto.getHeadCount())
-                    .category(reqDto.getCategory())
-                    .days(reqDto.getDay())
-                    .time(reqDto.getTime())
-                    .type(Type.mapPost)
-                    .build();
-        } else {
-            community = Community.builder()
-                    .name(reqDto.getName())
-                    .introduce(reqDto.getIntroduce())
-                    .headCount(reqDto.getHeadCount())
-                    .category(reqDto.getCategory())
-                    .days(reqDto.getDay())
-                    .time(reqDto.getTime())
-                    .type(Type.mapPost)
-                    .build();
+            imgUrl = "https://withplanner-s3.s3.ap-northeast-2.amazonaws.com/" + name;
         }
+
+        community = Community.builder()
+                .name(reqDto.getName())
+                .introduce(reqDto.getIntroduce())
+                .communityImg(imgUrl)
+                .headCount(reqDto.getHeadCount())
+                .category(reqDto.getCategory())
+                .days(reqDto.getDay())
+                .time(reqDto.getTime())
+                .type(Type.mapPost)
+                .build();
 
         user.addCommunity(community);
 
